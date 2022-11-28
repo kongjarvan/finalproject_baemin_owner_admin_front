4d22f641d2d7bb45b647ee28c2bc50fb2a4e0550
kongjarvan <107098014+kongjarvan@users.noreply.github.com>
2022-11-28 13:48:52 +0900

    가게등록 페이지 완료 (#5)
    * 로그인 페이지 완료
    
    * 입점신청 완료 (#4)
    
    * 가게등록페이지 완료

---
+++ lib/view/pages/main/component/store_info_menu.dart
@@ -0,0 +1,97 @@
+import 'package:baemin_owner_admin_front/constants.dart';
+import 'package:baemin_owner_admin_front/size.dart';
+import 'package:baemin_owner_admin_front/theme.dart';
+import 'package:baemin_owner_admin_front/view/pages/main/register_store/register_store_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class StoreInfoMenu extends StatefulWidget {
+  const StoreInfoMenu({Key? key}) : super(key: key);
+
+  @override
+  State<StoreInfoMenu> createState() => _StoreInfoMenuState();
+}
+
+class _StoreInfoMenuState extends State<StoreInfoMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Row(
+        children: [
+          Container(
+            color: kMenuBarMainColor,
+            child: Column(
+              children: [
+                Container(color: kMainColor, child: _buildInfoMenu1('가게등록')),
+                _buildInfoMenu1('가게정보'),
+                _buildInfoMenu2('메뉴관리'),
+                _buildInfoMenu2('리뷰관리'),
+              ],
+            ),
+          ),
+          Flexible(
+            child: RegisterStorePage(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoMenu1(text) {
+    return Padding(
+      padding: const EdgeInsets.all(gap_s),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 80,
+            child: InkWell(
+              onTap: () {},
+              child: Text(textAlign: TextAlign.end, '${text}', style: textTheme().headline3),
+            ),
+          ),
+          SizedBox(
+            width: 120,
+            child: Align(
+              alignment: AlignmentDirectional.centerEnd,
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(gap_xs),
+                  child: Icon(
+                    CupertinoIcons.chevron_down,
+                    size: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoMenu2(text) {
+    return Padding(
+      padding: const EdgeInsets.all(gap_s),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 80,
+            child: InkWell(
+              onTap: () {},
+              child: Text(
+                textAlign: TextAlign.end,
+                '${text}',
+                style: TextStyle(
+                  color: kMenuIconColor,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 120),
+        ],
+      ),
+    );
+  }
+}
