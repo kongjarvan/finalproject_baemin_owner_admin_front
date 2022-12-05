f0ff658e93864aa9f1125d190f0d69d88ca4b3c5
kongjarvan <107098014+kongjarvan@users.noreply.github.com>
2022-12-05 15:25:18 +0900

    사업자 등록 관리 페이지 완료 (#31)
    * 관리자 페이지 메인메뉴바 완성
    
    * 사업자 등록신청 페이지 완성

---
+++ lib/view/pages/admin/register_owner/admin_register_owner_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdminRegisterOwnerPage extends StatefulWidget {
+  const AdminRegisterOwnerPage({Key? key}) : super(key: key);
+
+  @override
+  State<AdminRegisterOwnerPage> createState() => _AdminRegisterOwnerPageState();
+}
+
+class _AdminRegisterOwnerPageState extends State<AdminRegisterOwnerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
