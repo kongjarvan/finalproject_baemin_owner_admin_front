206d14601e8a24f3c6c4935873194f72421091f6
kongjarvan <wjsdudwox@gmail.com>
2022-12-06 15:49:39 +0900

    가게정보 메뉴바 리팩토링 완료

--- lib/view/pages/main/component/store_info_menu.dart
+++ lib/view/pages/main/component/store_info_menu.dart
@@ -1,7 +1,10 @@
 import 'package:baemin_owner_admin_front/constants.dart';
 import 'package:baemin_owner_admin_front/size.dart';
 import 'package:baemin_owner_admin_front/theme.dart';
+import 'package:baemin_owner_admin_front/view/pages/main/menu_list/menu_list_page.dart';
+import 'package:baemin_owner_admin_front/view/pages/main/register_store/register_store_page.dart';
 import 'package:baemin_owner_admin_front/view/pages/main/review_list/review_list_page.dart';
+import 'package:baemin_owner_admin_front/view/pages/main/update_store/update_store_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -12,6 +15,17 @@ class StoreInfoMenu extends StatefulWidget {
   State<StoreInfoMenu> createState() => _StoreInfoMenuState();
 }
 
+final List<Widget> selectedMainView = [
+  RegisterStorePage(),
+  UpdateStorePage(),
+  MenuListPage(),
+  ReviewListPage(),
+];
+
+var _selectedIndex = 0;
+
+bool _isOpen = false;
+
 class _StoreInfoMenuState extends State<StoreInfoMenu> {
   @override
   Widget build(BuildContext context) {
@@ -20,83 +34,125 @@ class _StoreInfoMenuState extends State<StoreInfoMenu> {
         children: [
           Container(
             color: kMenuBarMainColor,
+            width: 232,
             child: Column(
               children: [
-                Container(color: kMainColor, child: _buildInfoMenu1('가게등록')),
-                _buildInfoMenu1('가게정보'),
-                _buildInfoMenu2('메뉴관리'),
-                _buildInfoMenu2('리뷰관리'),
+                _buildRegisterStoreMenu(),
+                _buildStoreManageMenu(),
               ],
             ),
           ),
           Flexible(
-            child: ReviewListPage(),
+            child: selectedMainView[_selectedIndex],
           ),
         ],
       ),
     );
   }
 
-  Widget _buildInfoMenu1(text) {
+  Widget _buildRegisterStoreMenu() {
     return Padding(
       padding: const EdgeInsets.all(gap_s),
       child: Row(
         children: [
-          SizedBox(
-            width: 80,
-            child: InkWell(
-              onTap: () {},
-              child: Text(textAlign: TextAlign.end, '${text}', style: textTheme().headline3),
+          InkWell(
+            onTap: () {
+              setState(() {
+                _selectedIndex = 0;
+              });
+            },
+            child: SizedBox(
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: gap_s),
+                child: Text(textAlign: TextAlign.start, '가게등록', style: textTheme().headline3),
+              ),
             ),
           ),
-          SizedBox(
-            width: 120,
-            child: Align(
-              alignment: AlignmentDirectional.centerEnd,
-              child: InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(gap_xs),
-                  child: Icon(
-                    CupertinoIcons.chevron_down,
-                    size: 16,
-                    color: Colors.white,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpdateStoreMenu() {
+    return Padding(
+      padding: const EdgeInsets.all(gap_s),
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                _selectedIndex = 1;
+              });
+            },
+            child: SizedBox(
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: gap_s),
+                child: Text(textAlign: TextAlign.start, '가게수정', style: textTheme().headline3),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStoreManageMenu() {
+    return SingleChildScrollView(
+      primary: false,
+      child: ExpansionPanelList(
+          elevation: 0,
+          expandedHeaderPadding: const EdgeInsets.all(0),
+          children: [
+            ExpansionPanel(
+              backgroundColor: kMenuBarMainColor,
+              canTapOnHeader: true,
+              headerBuilder: (context, isExpanded) {
+                return Padding(
+                  padding: const EdgeInsets.all(gap_s),
+                  child: SizedBox(
+                    width: 200,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: gap_s),
+                      child: Text(textAlign: TextAlign.start, '가게관리', style: textTheme().headline3),
+                    ),
                   ),
-                ),
+                );
+              },
+              body: Column(
+                children: [
+                  _buildInfoMenu('메뉴관리', 2),
+                  _buildInfoMenu('리뷰관리', 3),
+                ],
               ),
+              isExpanded: _isOpen,
             ),
-          ),
-        ],
-      ),
+          ],
+          expansionCallback: (panelIndex, isExpanded) {
+            setState(() {
+              _isOpen = !_isOpen;
+            });
+          }),
     );
   }
 
-  Widget _buildInfoMenu2(text) {
+  Widget _buildInfoMenu(text, index) {
     return Padding(
       padding: const EdgeInsets.all(gap_s),
-      child: Row(
-        children: [
-          SizedBox(
-            width: 80,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ReviewListPage(),
-                    ));
-              },
-              child: Text(
-                textAlign: TextAlign.end,
-                '${text}',
-                style: TextStyle(
-                  color: kMenuIconColor,
-                ),
-              ),
-            ),
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        child: SizedBox(
+          width: 200,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: gap_m),
+            child: Text('${text}', style: TextStyle(color: kMenuIconColor, fontSize: 16)),
           ),
-          SizedBox(width: 120),
-        ],
+        ),
       ),
     );
   }
